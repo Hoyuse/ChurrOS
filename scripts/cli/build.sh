970f3b68a1d87dd509e8ac87a1774be14149b17e
Jorge Ramos <freefirexxx26@gmail.com>
2026-07-13 13:36:16 -0500

    Solucion de bug con churros run

--- scripts/cli/build.sh
+++ scripts/cli/build.sh
@@ -7,16 +7,34 @@ echo "      ChurrOS Build System"
 echo "======================================"
 echo
 
-echo "[1/2] Cleaning previous build..."
+echo "[1/4] Preparing branding..."
+
+mkdir -p archiso/airootfs/root
+
+cp branding/customize_airootfs.sh \
+    archiso/airootfs/root/customize_airootfs.sh
+
+mkdir -p archiso/airootfs/root/branding
+
+cp -r branding/files \
+    archiso/airootfs/root/branding/
+
+echo "[2/4] Cleaning previous build..."
+
 sudo rm -rf work
 mkdir -p out
 
-echo "[2/2] Building ISO..."
+echo "[3/4] Building ISO..."
 
 sudo mkarchiso -v \
     -w work \
     -o out \
     archiso
+
+echo "[4/4] Cleaning temporary files..."
+
+rm -f archiso/airootfs/root/customize_airootfs.sh
+rm -rf archiso/airootfs/root/branding
 
 echo
 echo "======================================"
--- scripts/cli/run.sh
+++ scripts/cli/run.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+VM_DIR="vm"
+DISK="$VM_DIR/ChurrOS.qcow2"
+VARS="$VM_DIR/OVMF_VARS.fd"
+
 ISO=$(find out -name "*.iso" | head -n1)
 
 if [ -z "$ISO" ]; then
@@ -13,12 +17,32 @@ if [ -z "$ISO" ]; then
     ISO=$(find out -name "*.iso" | head -n1)
 fi
 
-echo "Launching QEMU..."
+mkdir -p "$VM_DIR"
+
+if [ ! -f "$DISK" ]; then
+
+    echo
+    echo "Creating development virtual machine..."
+    echo
+
+    qemu-img create -f qcow2 "$DISK" 64G
+
+    cp /usr/share/edk2/x64/OVMF_VARS.4m.fd "$VARS"
+
+fi
+
+echo
+echo "Launching ChurrOS Development VM..."
+echo
 
 qemu-system-x86_64 \
     -enable-kvm \
-    -m 4096 \
+    -machine q35,accel=kvm \
     -cpu host \
     -smp 4 \
-    -bios /usr/share/edk2/x64/OVMF.4m.fd \
-    -cdrom "$ISO"
+    -m 4096 \
+    -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
+    -drive if=pflash,format=raw,file="$VARS" \
+    -drive file="$DISK",format=qcow2,if=virtio \
+    -cdrom "$ISO" \
+    -boot order=d
