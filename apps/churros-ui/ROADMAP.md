c9d61fc0b82d5964e979ce896c903663919262bb
Jorge Ramos <freefirexxx26@gmail.com>
2026-07-13 20:58:44 -0500

    feat(ui): add first ChurrOS UI component

---
+++ apps/churros-ui/ROADMAP.md
@@ -0,0 +1,25 @@
+# Roadmap
+
+## v0.1
+
+- ActionCard
+- InfoCard
+- Header
+- Footer
+
+## v0.2
+
+- Sidebar
+- Dialogs
+- Buttons
+- Navigation
+
+## v0.3
+
+- Animaciones
+- Temas
+- Componentes avanzados
+
+## Futuro
+
+- Biblioteca completa para todas las aplicaciones oficiales.
---
+++ apps/churros-welcome/src/window.py
@@ -0,0 +1,54 @@
+import gi
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+
+from gi.repository import Gtk, Adw
+
+
+class ChurrOSWelcome(Adw.Application):
+
+    def __init__(self):
+        super().__init__(application_id="org.churros.Welcome")
+
+    def do_activate(self):
+
+        window = Adw.ApplicationWindow(application=self)
+
+        window.set_title("ChurrOS Welcome")
+        window.set_default_size(1000, 650)
+
+        header = Adw.HeaderBar()
+
+        title = Gtk.Label()
+        title.set_markup(
+            "<span size='xx-large' weight='bold'>ChurrOS</span>"
+        )
+
+        subtitle = Gtk.Label(
+            label="Bienvenido a ChurrOS\nUna distribución Linux creada por la comunidad."
+        )
+
+        subtitle.set_justify(Gtk.Justification.CENTER)
+
+        content = Gtk.Box(
+            orientation=Gtk.Orientation.VERTICAL,
+            spacing=18
+        )
+
+        content.set_halign(Gtk.Align.CENTER)
+        content.set_valign(Gtk.Align.CENTER)
+
+        content.append(title)
+        content.append(subtitle)
+
+        box = Gtk.Box(
+            orientation=Gtk.Orientation.VERTICAL
+        )
+
+        box.append(header)
+        box.append(content)
+
+        window.set_content(box)
+
+        window.present()
