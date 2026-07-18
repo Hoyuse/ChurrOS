a4f2aa28291cb50ea316859554f18f0fdca4750c
Jorge Ramos <freefirexxx26@gmail.com>
2026-07-18 09:43:26 -0500

    Agregar awww a churros welcome para que se ejecute el fondo al iniciar el sistema

--- archiso/airootfs/usr/share/churros/churros-welcome/churros-welcome.sh
+++ archiso/airootfs/usr/share/churros/churros-welcome/churros-welcome.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd /usr/share/churros/churros-welcome
+
+env > /tmp/churros-env.txt
+
+sleep 3
+
+env >> /tmp/churros-env.txt
+
+awww img /usr/share/churros/wallpapers/default.png --transition-type none \
+    > /tmp/awww.log 2>&1
 
 exec /usr/bin/python3 src/main.py
--- archiso/airootfs/usr/share/churros/churros-welcome/src/main.py
+++ archiso/airootfs/usr/share/churros/churros-welcome/src/main.py
@@ -1,13 +1,32 @@
+import subprocess
+import time
+
 from window import ChurrOSWelcome
 
 
 def main():
+    while subprocess.run(
+    ["/usr/bin/awww", "query"],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.DEVNULL,
+).returncode != 0:
+    time.sleep(0.2)
+
+    subprocess.Popen(
+        [
+            "/usr/bin/awww",
+            "img",
+            "/usr/share/churros/wallpapers/default.png",
+            "--transition-type",
+            "none",
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
 
     app = ChurrOSWelcome()
-
     app.run()
 
 
 if __name__ == "__main__":
-
     main()
