3af89be47fe7231f1f08c34bdd19f7a8e4191fa5
Hoyuse <freefirexxx26@gmail.com>
2026-09-26 10:07:58 -0500

    fix: resolve GRUB escape trap and greetd autologin issues
    - configure-greetd-session: added script to detect autologin group and dynamically set initial_session in config.toml
    - shellprocess-cleanup: replaced static greetd rewrite with configure-greetd-session call
    - users.conf: explicitly set autologinGroup so Calamares adds users appropriately
    - grubcfg.conf: set GRUB_TIMEOUT_STYLE to menu
    - shellprocess-grub-theme.conf: injected 'set superusers=""' via 01_churros_lock to disable GRUB shell

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -44,6 +44,7 @@ file_permissions=(
   ["/usr/share/churros/scripts/set-icons"]="0:0:755"
   ["/usr/share/churros/scripts/make-boot-grub-readable"]="0:0:755"
   ["/usr/share/churros/scripts/configure-greeter-locale"]="0:0:755"
+  ["/usr/share/churros/scripts/configure-greetd-session"]="0:0:755"
   ["/usr/share/icons/hicolor/scalable/apps/churros-welcome.svg"]="0:0:644"
   ["/usr/share/icons/hicolor/scalable/apps/churros-settings.svg"]="0:0:644"
   ["/usr/share/icons/hicolor/scalable/apps/churros-logo.svg"]="0:0:644"
